3d5aaf213050932a2e4455614011a126338fc257
Yasitha Bhanuka <yasithabhanukac@gmail.com>
2025-01-12 21:28:39 +0530

    Add Booking entity and update User entity for booking management

---
+++ src/main/java/com/yasithadev/yasithadev/entity/Booking.java
@@ -0,0 +1,63 @@
+package com.yasithadev.yasithadev.entity;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "bookings")
+public class Booking {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Check in date is required")
+    private LocalDate checkInDate;
+
+    @NotBlank(message = "Check out date must be in future")
+    private LocalDate checkOutDate;
+
+    @Min(value = 1, message = "Number of adults must be at less that 1")
+    private int numOfAdults;
+    
+    @Min(value = 0, message = "Number of children must be at less that 0")
+    private int numOfChildren;
+    
+    private int totalNumOfGuest;
+    
+    private String bookingConfimationCode;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    // @ManyToOne(fetch = FetchType.LAZY)
+    // @JoinColumn(name = "room_id")
+    // private Room room;
+
+    public void calculateTotalNumberOfGuest(){
+        this.totalNumOfGuest = this.numOfAdults + this.numOfChildren;
+    }
+
+    public void setNumOfAdults(int numOfAdults){
+        this.numOfAdults= numOfAdults;
+    }
+    
+    public void setNumOfChildren(int numOfChildren){
+        this.numOfChildren = numOfChildren;
+    }
+
+    
+
+}
--- src/main/java/com/yasithadev/yasithadev/entity/User.java
+++ src/main/java/com/yasithadev/yasithadev/entity/User.java
@@ -1,8 +1,11 @@
 package com.yasithadev.yasithadev.entity;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import jakarta.persistence.Column;
@@ -32,35 +35,30 @@ public class User implements UserDetails {
     private String phoneNumber;
     private String password;
     private String role;
-    //private List<Booking> bookings = new ArrayList<>();
+    private List<Booking> bookings = new ArrayList<>();
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        // TODO Auto-generated method stub
-        return null;
+        return List.of(new SimpleGrantedAuthority(role));
     }
     @Override
     public String getUsername() {
         // TODO Auto-generated method stub
-        return null;
+        return email;
     }
     @Override
     public boolean isAccountNonExpired() {
-        // TODO Auto-generated method stub
-        return UserDetails.super.isAccountNonExpired();
+        return true;
     }
     @Override
     public boolean isAccountNonLocked() {
-        // TODO Auto-generated method stub
-        return UserDetails.super.isAccountNonLocked();
+        return true;
     }
     @Override
     public boolean isCredentialsNonExpired() {
-        // TODO Auto-generated method stub
-        return UserDetails.super.isCredentialsNonExpired();
+        return true;
     }
     @Override
     public boolean isEnabled() {
-        // TODO Auto-generated method stub
-        return UserDetails.super.isEnabled();
+        return true;
     }
 }
