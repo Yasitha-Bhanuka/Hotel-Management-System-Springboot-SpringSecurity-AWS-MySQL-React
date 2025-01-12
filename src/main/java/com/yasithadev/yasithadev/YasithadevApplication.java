158234b2d49168acb072141ceac892368a65f6ee
Yasitha Bhanuka <yasithabhanukac@gmail.com>
2025-01-12 19:57:37 +0530

    Initialize Spring Boot project with basic configuration and testing setup

---
+++ src/main/java/com/yasithadev/yasithadev/YasithadevApplication.java
@@ -0,0 +1,13 @@
+package com.yasithadev.yasithadev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YasithadevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YasithadevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yasithadev/yasithadev/YasithadevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yasithadev.yasithadev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YasithadevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
