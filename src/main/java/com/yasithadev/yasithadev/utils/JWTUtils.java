713cc4f61e235f720f829ecd5b828283b69df6bc
Yasitha Bhanuka <yasithabhanukac@gmail.com>
2025-01-13 07:29:15 +0530

    Add JWTUtils class for handling JSON Web Token creation and management

---
+++ src/main/java/com/yasithadev/yasithadev/utils/JWTUtils.java
@@ -0,0 +1,25 @@
+package com.yasithadev.yasithadev.utils;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JWTUtils {
+
+    private static final long EXPIRATION_TIME = 1000*60*24*7; // 7 days
+
+    private final SecretKey Key;
+
+    public JWTUtils() {
+
+        String secretString = "843567893696976453275974432697R634976R738467TR678T34865R6834R8763T478378637664538745673865783678548735687R3";
+        byte[] keyBytes = Base64.getDecoder().decode(secretString.getBytes(StandardCharsets.UTF_8));
+        this.Key = new SecretKeySpec(keyBytes, "HmacSHA256");
+    }
+
+}
