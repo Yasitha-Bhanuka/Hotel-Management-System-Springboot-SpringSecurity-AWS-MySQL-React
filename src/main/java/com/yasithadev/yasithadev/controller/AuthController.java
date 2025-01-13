2f0a786d189606fd6eb101b464b7f2b2cfb44ca4
Yasitha Bhanuka <yasithabhanukac@gmail.com>
2025-01-13 12:25:52 +0530

    Add AuthController for user registration and login functionalities

---
+++ src/main/java/com/yasithadev/yasithadev/controller/AuthController.java
@@ -0,0 +1,33 @@
+package com.yasithadev.yasithadev.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.yasithadev.yasithadev.dto.LoginRequest;
+import com.yasithadev.yasithadev.dto.Response;
+import com.yasithadev.yasithadev.entity.User;
+import com.yasithadev.yasithadev.service.interfac.IUserService;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    @Autowired
+    private IUserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<Response> register(@RequestBody User user) {
+        Response response = userService.register(user);
+        return ResponseEntity.status(response.getStatusCode()).body(response);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<Response> login(@RequestBody LoginRequest loginRequest) {
+        Response response = userService.login(loginRequest);
+        return ResponseEntity.status(response.getStatusCode()).body(response);
+    }
+}
